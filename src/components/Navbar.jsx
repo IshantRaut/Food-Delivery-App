f51c2821937147d6b198d019aa53043856530034
IshantRaut <ishjee2018@gmail.com>
2025-02-25 15:39:18 +0530

    Food deleivery update icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,31 @@ import { setSearch } from "../redux/slices/SearchSlice";
 
 const Navbar = () => {
   const dispatch = useDispatch();
+
   return (
-    <nav className="flex flex-col lg:flex-row justify-between py-3 mx-6 mb-10">
-      <div>
-        <h3 className="text-xl font-bold text-gray-600">
-          {new Date().toUTCString().slice(0, 16)}
-        </h3>
-        <h1 className="text-2xl font-bold ">Flavoro Foods</h1>
+    <nav className="bg-gray-100 flex flex-col lg:flex-row items-center justify-between py-3 px-6 mb-10 shadow-md rounded-lg relative">
+      {/* Date (Moved to top-left corner) */}
+      <h3 className="text-sm text-gray-600 absolute top-2 left-4  lg:block">
+        {new Date().toUTCString().slice(0, 16)}
+      </h3>
+
+      {/* Logo (Centered in mobile view) */}
+      <div className="flex flex-col items-center lg:flex-row lg:items-center lg:gap-4 w-full">
+        <img 
+          src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent-2048x2048.png" 
+          className="w-20 lg:w-28"
+        />
       </div>
-      <div>
+
+      {/* Search Bar */}
+      <div className="w-full lg:w-[25vw] mt-4 lg:mt-0">
         <input
           type="search"
           name="search"
-          id=""
-          placeholder="Search here"
+          placeholder="Search here..."
           autoComplete="off"
           onChange={(e) => dispatch(setSearch(e.target.value))}
-          className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[25vw]"
+          className="p-2 border border-gray-400 bg-gray-50 text-sm rounded-lg outline-none w-full focus:border-gray-600"
         />
       </div>
     </nav>
